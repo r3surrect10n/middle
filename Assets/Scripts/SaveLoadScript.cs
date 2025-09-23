342518d5aabc47aaf584d11806cb66fbb2cdc9a9
r3suurect10n <sosnin.a.a@yandex.ru>
2025-09-23 16:56:27 +0300

    local savings

--- Assets/Scripts/SaveLoadScript.cs
+++ Assets/Scripts/SaveLoadScript.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,32 +10,89 @@ public class SaveLoadScript : MonoBehaviour
     [SerializeField] private Text _enteredText;
     [SerializeField] private Text _enteredNums;
     [SerializeField] private Text _loadedText;
-    [SerializeField] private Text _loadedNums;   
+    [SerializeField] private Text _loadedNums;
+    [SerializeField] private Text _gdLoadedText;
+    [SerializeField] private Text _gdLoadedNums;
+
+    private string _filePath;
 
     private void Start()
-    { 
-        if (GoogleDriveTools.FileList().Count <= 0)
-        {
-            _loadedText.text = "Данные отсутствуют";
-            _loadedNums.text = "Данные отсутствуют";
-        }
-        else
-        {
+    {
+        _filePath = Path.Combine(Application.persistentDataPath, "gameData.json");
 
-        }
+        CheckData();
 
-        Debug.Log(GoogleDriveTools.FileList().Count);
+        //Debug.Log(GoogleDriveTools.FileList().Count);
     }
 
     public void OnEnterClick()
     {
-        _enteredText.text = _textField.text;
-        _enteredNums.text = _numField.text;
+        if (_textField.text != "" && _numField.text != "")
+        {
+            _enteredText.text = _textField.text;
+            _enteredNums.text = _numField.text;
+        }
+        else
+            Debug.Log("Заполните все поля!");
+        
     }
 
     public void OnSaveClick()
     {
-        GoogleDriveTools.Upload(_enteredText.text);
+        SaveData();
+
+        GoogleDriveTools.Upload(_filePath);
+        
         Debug.Log("Saved");
+    }
+
+    public void OnLoadClick()
+    {
+        LoadData();
+    }
+
+    private void SaveData()
+    {
+        GameData gameData = new GameData();
+
+        if (_enteredText.text != "" && _enteredNums.text != "")
+        {
+            gameData.SavedText = _enteredText.text;
+            gameData.SavedNums = _enteredNums.text;
+
+            string json = JsonUtility.ToJson(gameData, true);
+
+            File.WriteAllText(_filePath, json);
+        }
+
+    }
+
+    private void LoadData()
+    {
+        if (File.Exists(_filePath))
+        {
+            string json = File.ReadAllText(_filePath);
+
+            GameData gameData = JsonUtility.FromJson<GameData>(json);
+
+            _loadedText.text = gameData.SavedText;
+            _loadedNums.text = gameData.SavedNums;
+        }
+    }
+
+    private void CheckData()
+    {
+        if (GoogleDriveTools.FileList().Count <= 0)
+        {
+            _gdLoadedText.text = "Данные отсутствуют";
+            _gdLoadedNums.text = "Данные отсутствуют";
+        }
+        else        
+
+        if (!File.Exists(_filePath))
+        {
+            _loadedText.text = "Данные отсутствуют";
+            _loadedNums.text = "Данные отсутствуют";
+        }
     }
 }
