a61030e5c2dae70ffd6622ecb53b7ecb5087dd9e
r3suurect10n <sosnin.a.a@yandex.ru>
2025-09-24 23:36:19 +0300

    Update movement

--- Assets/Project/Scripts/Player/PlayerMovement.cs
+++ Assets/Project/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -18,13 +19,34 @@ public class PlayerMovement : MonoBehaviour
     {
         _movement = callbackContext.ReadValue<Vector2>();
 
-        if (_movement.x > 0 || _movement.y > 0)
+        if (callbackContext.performed)
+        {
+            StopAllCoroutines();
+            StartCoroutine(ContinousMovement());
+        }
+
+        if (callbackContext.canceled)
+            StopAllCoroutines();
+
+    }
+
+    private IEnumerator ContinousMovement()
+    {
+        while (true)
+        {
             MoveCharacter();
+            yield return new WaitForFixedUpdate();
+        }
     }
 
     private void MoveCharacter()
     {
         Vector3 motion = new Vector3(_movement.x * _playerSpeed * Time.deltaTime, 0f, _movement.y * _playerSpeed * Time.deltaTime);
         _controller.Move(motion);
+    }
+
+    private void CharacterRotation()
+    {
+        
     }
 }
