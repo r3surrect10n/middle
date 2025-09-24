74974912d43245b0d19575b29bd32e966cb532e1
r3suurect10n <sosnin.a.a@yandex.ru>
2025-09-24 23:17:45 +0300

    Starting character controller, add assets

---
+++ Assets/Project/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _playerSpeed;
+
+    private CharacterController _controller;
+
+    private Vector2 _movement;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    public void MoveAction(InputAction.CallbackContext callbackContext)
+    {
+        _movement = callbackContext.ReadValue<Vector2>();
+
+        if (_movement.x > 0 || _movement.y > 0)
+            MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        Vector3 motion = new Vector3(_movement.x * _playerSpeed * Time.deltaTime, 0f, _movement.y * _playerSpeed * Time.deltaTime);
+        _controller.Move(motion);
+    }
+}
