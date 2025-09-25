e0bf9bca3df628b607c8ef214de6be83fbb17383
r3suurect10n <sosnin.a.a@yandex.ru>
2025-09-25 16:51:29 +0300

    Add anims

--- Assets/Project/Scripts/Player/PlayerMovement.cs
+++ Assets/Project/Scripts/Player/PlayerMovement.cs
@@ -43,10 +43,16 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 motion = new Vector3(_movement.x * _playerSpeed * Time.deltaTime, 0f, _movement.y * _playerSpeed * Time.deltaTime);
         _controller.Move(motion);
+
+        CharacterRotation();
     }
 
     private void CharacterRotation()
     {
-        
+        // transform.Rotate(transform.rotation.x, _movement.x + _movement.y, transform.rotation.z);
+
+        Vector2 targetRotationY = new Vector2(_movement.x, _movement.y);
+
+        transform.localRotation = Quaternion.LookRotation(new Vector3(_movement.x, 0, _movement.y));
     }
 }
