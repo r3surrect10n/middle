f18b7cfe1a1a13214cc60d4901a28b43f0eb080e
r3suurect10n <sosnin.a.a@yandex.ru>
2025-09-26 16:49:16 +0300

    Add assets

--- Assets/Project/Scripts/Player/PlayerMovement.cs
+++ Assets/Project/Scripts/Player/PlayerMovement.cs
@@ -6,9 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _playerSpeed;
     [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _gravityForce;
 
     private CharacterController _controller;
 
+    private Coroutine _movementCoroutine;
+    private Coroutine _gravityCoroutine;
+    
+    private Vector3 direction;
     private Vector2 _movement;
 
     private bool _isMoving;
@@ -16,25 +21,34 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         _controller = GetComponent<CharacterController>();
+
+        _gravityCoroutine = StartCoroutine(ApplyGravity());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
     }
 
     public void MoveAction(InputAction.CallbackContext callbackContext)
     {
         _movement = callbackContext.ReadValue<Vector2>();
 
-        if (callbackContext.performed && _movement.magnitude > 0.1f)
+        if (callbackContext.performed && _movement.magnitude > 0.05)
         {
             _isMoving = true;
 
-            StopAllCoroutines();
-            StartCoroutine(ContinousMovement());
+            if (_movementCoroutine != null)
+                StopCoroutine(_movementCoroutine);
+
+            _movementCoroutine = StartCoroutine(ContinousMovement());
         }
 
-        if (callbackContext.canceled || _movement.magnitude <= 0.1f)
+        if (callbackContext.canceled || _movement.magnitude <= 0.05)
         {
             _isMoving = false;
 
-            StopAllCoroutines();
+            StopCoroutine(_movementCoroutine);
         }
     }
 
@@ -42,19 +56,26 @@ public class PlayerMovement : MonoBehaviour
     {
         while (_isMoving)
         {
-            MoveCharacter();
-            yield return new WaitForFixedUpdate();
+            MoveCharacter();            
+            yield return null;
         }
     }
 
+    private IEnumerator ApplyGravity()
+    {
+        while (true)
+        {
+            Vector3 gravity = Vector3.down * _gravityForce;
+
+            _controller.Move(gravity);
+            yield return null;
+        }
+    }    
+
     private void MoveCharacter()
     {
-        Vector3 direction = new Vector3(_movement.x, Vector3.zero.y, _movement.y).normalized;
-
-        Vector3 motion = _playerSpeed * direction * Time.fixedDeltaTime;
-
-        Debug.Log(motion);
-
+        direction = new Vector3(_movement.x, Vector3.zero.y, _movement.y).normalized;   
+        Vector3 motion = _playerSpeed * direction * Time.deltaTime;        
         _controller.Move(motion);
 
         CharacterRotation();
@@ -62,10 +83,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void CharacterRotation()
     {
-        Vector3 direction = new Vector3(_movement.x, Vector3.zero.y, _movement.y);
-
         Quaternion targetRotation = Quaternion.LookRotation(direction);
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.fixedDeltaTime);        
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);        
     }
 }
